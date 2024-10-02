9967471b4e030ededf06f39ace1decbca534001d
Krishnan Winter <krishnanwinter1@gmail.com>
2024-10-02 13:29:52 +1000

    cleanup
    Signed-off-by: Krishnan Winter <krishnanwinter1@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,6 @@ use goblin::pe::{
     PE,
 };
 
-#[no_mangle]
-#[link_section = ".loader"]
-#[used]
-pub static bytes: [u8; 2535424] = [0; 2535424];
-
 const load_addr_str: &str = "0x100000000";
 
 #[global_allocator]
@@ -29,11 +24,12 @@ static ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 #[entry]
 fn main(_image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
-    let x = 1;
-    let ptr_y = &x;
     uefi::helpers::init().unwrap();
 
-    /* Allocating our own heap due to the absecnce of a heap allocator in no_std. */
+    /* Get the load address that the loader image expected to be copied into. */
+    let load_addr: u64 = u64::from_str_radix(load_addr_str.strip_prefix("0x").unwrap(), 16).expect("Load address was not a valid hex string");
+
+    /* Allocating our own heap due to the absecnce of a heap allocator in no_std. This is required by the Goblin crate. */
     let heap_res = system_table.boot_services().allocate_pages(AllocateType::AnyPages, MemoryType::LOADER_DATA, 10).expect("Failed to alloc");
 
     unsafe {
@@ -43,22 +39,9 @@ fn main(_image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     }
 
     /* Open the protocol */
-    let proto: ScopedProtocol<LoadedImage> =
-        open_protocol_exclusive(_image_handle).unwrap();
-    info!("Successfully unwrapped protocol. fdsThis path {:?} is this info: {:?}", proto.file_path(), proto.info());
-
+    let proto: ScopedProtocol<LoadedImage> = open_protocol_exclusive(_image_handle).unwrap();
+    /* Get the image info. This will include name, virtual address, size, etc... */
     let image_info: (*const core::ffi::c_void, u64) = proto.info();
-
-    let load_addr: u64 = u64::from_str_radix(load_addr_str.strip_prefix("0x").unwrap(), 16).expect("Load address was not a valid hex string");
-    /* Allocate the region that we will load the image into */
-    // TODO: What if this conflicts with where UEFI has loaded us (or something else that UEFI is using?)
-    // We should:
-    //      1. Allocate a random region of memory with size that is big enough to store the image
-    //      2. Load the image there
-    //      3. Have an assembly routine at the start of the image that relocates it to where it is meant to be executing prior to jumping to main.
-    let allocate_res = system_table.boot_services().allocate_pages(AllocateType::Address(load_addr), MemoryType::LOADER_DATA, (bytes.len() / 4096) + 1).expect("Failed to alloc");
-
-
 
     let sl = unsafe { slice::from_raw_parts(image_info.0 as *mut u8, image_info.1 as usize) };
 
@@ -67,25 +50,24 @@ fn main(_image_handle: Handle, system_table: SystemTable<Boot>) -> Status {
     for section in pe_parsed.sections {
         let name = unsafe{ CStr::from_ptr(section.name.as_ptr() as *const i8) };
         if name.to_str().unwrap() == ".mloader" {
-            info!("BOOM::: {}", name.to_str().unwrap());
-            info!("This is the vaddr: {:x}", image_info.0 as u64 + section.virtual_address as u64);
+            /* Allocate the region that we will load the image into */
+            // TODO: What if this conflicts with where UEFI has loaded us (or something else that UEFI is using?)
+            // We should:
+            //      1. Allocate a random region of memory with size that is big enough to store the image
+            //      2. Load the image there
+            //      3. Have an assembly routine at the start of the image that relocates it to where it is meant to be executing prior to jumping to main.
+            let allocate_res = system_table.boot_services().allocate_pages(AllocateType::Address(load_addr), MemoryType::LOADER_DATA, (section.virtual_size as usize / 4096) + 1).expect("Failed to alloc");
             /* Define the region as a slice so that we can access it */
             let load_region: &mut [u8] = unsafe {slice::from_raw_parts_mut(load_addr as *mut u8, section.virtual_size as usize)};
-
+            /* Define the mloader section as a slice as well. */
             let section_data: &mut [u8] = unsafe {slice::from_raw_parts_mut((image_info.0 as u64 + section.virtual_address as u64) as *mut u8, section.virtual_size as usize)};
-
+            /* Copy the image into the correct memory region */
             load_region.copy_from_slice(&section_data);
-
-            for i in 0..100 {
-                info!(" this is byte : {} ---- {}", i, load_region[i]);
-            }
         }
     }
 
     /* Exit boot services  */
     unsafe{system_table.exit_boot_services(MemoryType::LOADER_DATA)};
-
-    /* Copy the image into the correct memory region */
 
     /* Jump to the loader  */
     let kernel_start: unsafe extern "C" fn() = unsafe { core::mem::transmute(load_addr)};
